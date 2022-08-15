3e7c5391b92a93713d7ffccec760aec8b0e629bd
gokulr-05 <99526288+gokulr-05@users.noreply.github.com>
2022-08-15 08:44:25 +0530

    Add files via upload

---
+++ STDIN/test1/acceptedCode.js
@@ -0,0 +1,42 @@
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+// your code goes here
+
+process.stdin.on("data", cacheInput).on("end", main);
+
+let s = "",
+  arr = [];
+
+function cacheInput(data) {
+  s = s.concat(data);
+}
+
+function prepareInput() {
+  arr = s.split(/\s+/).map((val) => {
+    return parseInt(val);
+  });
+}
+
+function main() {
+  prepareInput();
+
+  let testcase = arr.splice(0, 1)[0];
+
+  for (let i = 0; i < testcase; ++i) {
+    let count = 0;
+    let n = arr.splice(0, 1)[0];
+    let array1 = arr.splice(0, n);
+
+    for (let j = n - 2; j >= 0; --j) {
+      while (array1[j] >= array1[j + 1] && array1[j] > 0) {
+        --array1[j];
+        ++count;
+      }
+    }
+
+    // process.stdout.write(count + "\n");
+    console.log(count + "\n");
+  }
+  process.exit();
+}
